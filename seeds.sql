4b802a229aa6a26fa7c9f096ded0bb8621d1f380
Audra A Kosinski <57331762+ksaudra2010@users.noreply.github.com>
2020-05-27 22:07:21 -0500

    Update seeds.sql

--- seeds.sql
+++ seeds.sql
@@ -3,5 +3,3 @@ INSERT INTO burgers (burgername) VALUES ('Original Burger');
 INSERT INTO burgers (burgername) VALUES ('Spicy Sriracha Burger');
 
 INSERT INTO burgers (burgername) VALUES ('Impossible Burger');
-
-
